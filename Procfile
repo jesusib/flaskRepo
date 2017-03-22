ef51c55c433f975b3f8019bc1963ff83d478ce26
jesus.ib <jesus.igleber@gmail.com>
2017-03-22 13:17:09 +0100

    procife python

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web gunicorn  hello:app -b 0.0.0.0:8000
+web python hello
--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, url_for
+import os
 
 app = Flask(__name__)
 app.secret_key = 'random string'
@@ -20,3 +21,8 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', error = error)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

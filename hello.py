41bf7c6b70df044887163148bc2b8129b5a3bf14
jesus.ib <jesus.igleber@gmail.com>
2017-03-22 13:14:11 +0100

    .

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, url_fo
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 app.secret_key = 'random string'

b31e1ea3fc1f1e1f2fdc72e6a2eb5e75fded6b81
jesus.ib <jesus.igleber@gmail.com>
2017-03-22 12:49:54 +0100

    change hello import

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
-import os
+from flask import Flask, flash, redirect, render_template, request, url_fo
 app = Flask(__name__)
 app.secret_key = 'random string'
 
@@ -10,7 +9,7 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     error = None
-    
+
     if request.method == 'POST':
         if request.form['username'] != 'admin' or \
         request.form['password'] != 'admin':
@@ -20,4 +19,3 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', error = error)
-

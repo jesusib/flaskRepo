8a750e00258a90bee47f78ec6c9676897daa8e68
jesus.ib <jesus.igleber@gmail.com>
2017-03-22 12:10:58 +0100

    comit heroku

--- src/hello.py
+++ src/hello.py
@@ -21,7 +21,3 @@ def login():
 
     return render_template('login.html', error = error)
 
-if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)

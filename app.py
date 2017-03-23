b8eeccd1c03f2766cd18b21b8e956d2dc7a66955
jesus.ib <jesus.igleber@gmail.com>
2017-03-23 16:09:49 +0100

    added enable url

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 from flask import Flask, flash, redirect, render_template, request, url_for, jsonify
-import os
 from src import urls
+import os
+from flask.helpers import send_file, send_from_directory
+
 
 app = Flask(__name__)
 app.secret_key = 'random string'
 
+def emptyOK():
+    resp = jsonify()
+    resp.status_code = 200
+    return resp
+
+#Keep until dev is completed
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/enable/')
+def checkStatus():
+    return emptyOK()
 
 @app.route('/create/<instance>')
 def createInstance(instance):
     data = {
         'recordDefinition': {
             'ContactId':'{{Contact.Id}}',
-            'email':'{{Contact.Field(C_EmailAddress'
+            'email':'{{Contact.Field(C_EmailAddress)'
             }
         }
     resp = jsonify(data)
@@ -22,18 +34,17 @@ def createInstance(instance):
     return resp
 
 
-@app.route('/checkStatus/')
-def checkStatus():
-    data = {}
-    resp = jsonify(data)
-    resp.status_code = 200
-    return resp
+@app.route('/icon/')
+def icon():
+    img = 'templates'
+    return send_from_directory('templates', 'icon.png')
+
 
 
 if __name__ == '__main__':
     #Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
     
-   #app.debug = True
-    #app.run()
+   app.debug = True
+   app.run()

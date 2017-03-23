8095bc701e39b5dc1c66ba8b321a6e716be63c27
jesus.ib <jesus.igleber@gmail.com>
2017-03-23 16:11:45 +0100

    added enable url

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def icon():
 
 
 if __name__ == '__main__':
-    Bind to PORT if defined, otherwise default to 5000.
+    #Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     

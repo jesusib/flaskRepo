afd8e539502db7578e96e8e8eee4d079c4aa165a
jesus.ib <jesus.igleber@gmail.com>
2017-03-23 15:28:58 +0100

    added status url

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for, jsonify
 import os
 from src import urls
+
 app = Flask(__name__)
 app.secret_key = 'random string'
 
@@ -8,16 +9,31 @@ app.secret_key = 'random string'
 def index():
     return render_template('index.html')
 
-@app.route('/testRoute')
-def testRoute():
-    return urls.testRute()
+@app.route('/create/<instance>')
+def createInstance(instance):
+    data = {
+        'recordDefinition': {
+            'ContactId':'{{Contact.Id}}',
+            'email':'{{Contact.Field(C_EmailAddress'
+            }
+        }
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
 
 
-
+@app.route('/checkStatus/')
+def checkStatus():
+    data = {}
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    #Bind to PORT if defined, otherwise default to 5000.
+    '''port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    '''
+    app.debug = True
     app.run()

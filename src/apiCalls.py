0f8f97671ecf2176dcb60e899d9752de0c8eb72c
jesus.ib <jesus.igleber@gmail.com>
2017-03-31 11:52:47 +0200

    funciones de crear y modificar instancia

--- src/apiCalls.py
+++ src/apiCalls.py
@@ -26,11 +26,30 @@ def createStep(instanceId):
     res = requests.post(url,json=jsonReq, headers=headerAuth())
     return res
 
-def saveStep(instanceId, type, subtype, sr):
-    url = "https://secure.p03.eloqua.com"+"/api/REST/1.0/assets/contact/segment/123"
-    res = requests.get(url,headers=headerAuth())
+def saveStep(instanceName, type, subtype, sr):
+    
+    searchUrl = "https://secure.p03.eloqua.com"+"/api/REST/2.0/data/customObject/31/instances?search="+instanceName
+    searchRes = requests.get(searchUrl,headers=headerAuth())
+    searchResId = searchRes.json()['elements'][0]['id']
+    
+    url = "https://secure.p03.eloqua.com"+"/api/REST/2.0/data/customObject/31/instance/"+searchResId
+    updateInfo = {
+    "type": "CustomObjectData",
+    "fieldValues": [
+        {
+            "id": "388",
+            "value": type
+        },
+        {
+            "id": "389",
+            "value": subtype
+         },
+         {
+            "id": "390",
+            "value": sr
+         },
+        
+    ]
+    }
+    res = requests.put(url,json=updateInfo,headers=headerAuth())
     return res
-
-    print res.status_code
-    print res.headers['content-type']
-    print json.dumps(res.json(), indent=4)
